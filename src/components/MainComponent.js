112c689dee9fa376cf7cfab624b06f6ab758f513
i-pvg <parthvineelgarg3024@gmail.com>
2020-05-14 00:53:55 +0530

    Single Page Applications Part 1

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -5,6 +5,10 @@ import Header from './HeaderComponent';
 import Home from './HomeComponent';
 import Footer from './FooterComponent';
 import {DISHES} from '../shared/dishes';
+import {COMMENTS} from '../shared/comments';
+import {LEADERS} from '../shared/leaders';
+import {PROMOTIONS} from '../shared/promotions';
+
 import { Switch, Route, Redirect} from 'react-router-dom';
 import Contact from './ContactComponent';
 class Main extends Component {
@@ -13,6 +17,9 @@ class Main extends Component {
     super(props);
     this.state = {
       dishes:DISHES,
+      comments:COMMENTS,
+      promotions:PROMOTIONS,
+      leaders:LEADERS
     };
 
   }
@@ -21,7 +28,10 @@ class Main extends Component {
 
     const HomePage = () => {
       return(
-          <Home />
+          <Home dish={this.state.dishes.filter( (dish) => dish.featured)[0]} 
+                leader={this.state.leaders.filter( (leader) => leader.featured)[0]} 
+                promotion={this.state.dishes.filter( (promo) => promo.featured)[0]} 
+          />
         );
     };
     return (
