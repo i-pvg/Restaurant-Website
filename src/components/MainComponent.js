4b41c9a2cd784a36ba259e30feaeda8496af9bcc
i-pvg <parthvineelgarg3024@gmail.com>
2020-05-14 00:30:24 +0530

    React Router

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -2,34 +2,42 @@ import React, { Component } from 'react';
 import Menu from './MenuComponents';
 import Dishdetail from './DishDetailComponent';
 import Header from './HeaderComponent';
+import Home from './HomeComponent';
 import Footer from './FooterComponent';
 import {DISHES} from '../shared/dishes';
-
+import { Switch, Route, Redirect} from 'react-router-dom';
+import Contact from './ContactComponent';
 class Main extends Component {
 
   constructor(props){
     super(props);
     this.state = {
       dishes:DISHES,
-      selectedDish: null
     };
 
   }
-  
-  onSelectedDish(dishId) {
-        this.setState({
-            selectedDish: dishId
-        })
-  }
 
   render() {
+
+    const HomePage = () => {
+      return(
+          <Home />
+        );
+    };
     return (
       <div>
         <Header />
-        <Menu dishes={this.state.dishes}
-          onClick={(dishId) => this.onSelectedDish(dishId)} />
-        <Dishdetail 
-          dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
+        <Switch>
+          <Route path="/home" component={HomePage} />
+          <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
+          <Route exact path="/contactus" component={Contact} />
+          <Redirect to="/home" />
+
+//If we just wanted to return a component without passing any props
+//to it we can simply write component={Menu}, but to pass props, we 
+//write it through arrow funtion
+        </Switch>
+
         <Footer />
       </div>
     );
